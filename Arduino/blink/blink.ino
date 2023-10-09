e8a92bb7f02241d07c2aad42377a15f4a6336403
Sergio <sergio.pantalone@protonmail.com>
2023-10-09 00:43:14 -0400

    added lie detector

--- Arduino/blink/blink.ino
+++ Arduino/blink/blink.ino
@@ -1,14 +1,18 @@
-int varA = 6;
-int varB = varA; // 6
+/*
+ * Blink an LED off and on for 1 second
+ */
 
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  int varC = 0;
-  int varB = 3;
-  varA = varA + varB; // 9 (6 + 3)
-  int varC = varA; //9
+  // initialize digital pin 12 as an output
+  pinMode(12, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  int varC = 3;
-  varA = varA + varB + varC; // 18 (9 + 6 + 3)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
